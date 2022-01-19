95ec176a8202be73def6e023a1c0832051c23b6e
Lukasz Szypulewski <szyp86@gmail.com>
2022-01-19 20:30:03 +0100

    add navbar and some styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Main } from "./Pages/Main/main";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
